f825fb8449b078db7373ba0ceba6f50424abfa9a
Artexis10 <54939745+Artexis10@users.noreply.github.com>
2020-02-06 23:52:42 +0200

    final commit

--- server.js
+++ server.js
@@ -9,42 +9,49 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-    //checking user chosen currency
+    //check the user's choice
     let currency = req.body.currency;
-    let url = `https://api.coindesk.com/v1/bpi/currentvalue/${currency}.json`;
-    //get BitCoin currency values from external API
+    let url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`;
+    //get bitcoin rates from external API
     request(url, function(error, response, body){
         console.log("Status Message: ", response.statusMessage);      
         console.log("Server Status Code: ", response.statusCode);
-        //server response (the data we need)
+        //server response (the data we actually need)
         console.log(response.body);
 
         //convert response string to json object
         let data = JSON.parse(response.body);
         let Bitcoin = parseFloat(req.body.Bitcoin);
-        let bitcoin_value;
-        let value;
+        let own;
+        let price;
 
         if(currency === "EUR"){
-            value = parseFloat(data.bpi.EUR.rate_float);
-            bitcoin_value = value*Bitcoin;
-            console.log(bitcoin_value);
-            bitcoin_value = Math.round(bitcoin_value);
-            console.log("Value in EUR ", bitcoin_value);
+            price = parseFloat(data.bpi.EUR.rate_float);
+            own = price*Bitcoin;
+            console.log(own);
+            own = Math.round(own);
+            console.log("Price in EUR ", own);
         } else {
-            value = parseFloat(data.bpi.USD.rate_float);
-            bitcoin_value = value*Bitcoin;
-            console.log(bitcoin_value);
-            bitcoin_value = Math.round(bitcoin_value);
-            console.log("Value in USD ", bitcoin_value);
+            price = parseFloat(data.bpi.USD.rate_float);
+            own = price*Bitcoin;
+            console.log(own);
+            own = Math.round(own);
+            console.log("Price in USD ", own);
         }
         
         let disclaimer = data.disclaimer;
-        res.write(`${disclaimer}. `);
-        res.write(`Current value in ${currency} is ${bitcoin_value}.`);
+        res.write(`${disclaimer}`);
+
+        res.write('<br>');
+        res.write(`Current price in ${currency} is ${own}`);
         res.send();
+
+        
     });
+
+
 });
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
