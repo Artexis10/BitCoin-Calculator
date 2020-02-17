7c605f685cd2cf23e987650b1eb6fc24ca50ec1d
Artexis10 <54939745+Artexis10@users.noreply.github.com>
2020-02-17 15:41:44 +0200

    final commit

--- server.js
+++ server.js
@@ -53,13 +53,6 @@ app.post('/', function(req, res){
 
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
-
-/*
 app.listen(process.env.port || 3000, function(){
     console.log("Server has started.");
 });
